203b23219d0fbfde0b55ce374d34568ea25b40f7
Patrice De Saint Steban <patrice@desaintsteban.fr>
2022-02-17 01:13:47 +0100

    Update flutter and autoroute

--- lib/router.gr.dart
+++ lib/router.gr.dart
@@ -1,114 +1,138 @@
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 // **************************************************************************
 // AutoRouteGenerator
 // **************************************************************************
+//
+// ignore_for_file: type=lint
 
-import 'package:auto_route/auto_route.dart' as _i1;
-import 'package:flutter/material.dart' as _i2;
+import 'package:auto_route/auto_route.dart' as _i2;
+import 'package:flutter/material.dart' as _i8;
 
-import 'app.dart' as _i3;
-import 'pages/about_page.dart' as _i8;
-import 'pages/accountpage.dart' as _i7;
-import 'pages/detail_page.dart' as _i5;
-import 'pages/loanspage.dart' as _i6;
-import 'pages/search_page.dart' as _i4;
+import 'app.dart' as _i1;
+import 'pages/about_page.dart' as _i7;
+import 'pages/accountpage.dart' as _i6;
+import 'pages/detail_page.dart' as _i4;
+import 'pages/loanspage.dart' as _i5;
+import 'pages/search_page.dart' as _i3;
 
-class AppRouter extends _i1.RootStackRouter {
-  AppRouter([_i2.GlobalKey<_i2.NavigatorState>? navigatorKey])
+class AppRouter extends _i2.RootStackRouter {
+  AppRouter([_i8.GlobalKey<_i8.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i1.PageFactory> pagesMap = {
-    AppWidget.name: (routeData) => _i1.MaterialPageX<dynamic>(
-        routeData: routeData,
-        builder: (data) {
-          final args =
-              data.argsAs<AppWidgetArgs>(orElse: () => const AppWidgetArgs());
-          return _i3.AppWidget(key: args.key);
-        }),
-    SearchRouter.name: (routeData) => _i1.MaterialPageX<dynamic>(
-        routeData: routeData,
-        builder: (_) {
-          return const _i1.EmptyRouterPage();
-        }),
-    LoansRouter.name: (routeData) => _i1.MaterialPageX<dynamic>(
-        routeData: routeData,
-        builder: (_) {
-          return const _i1.EmptyRouterPage();
-        }),
-    AccountRouter.name: (routeData) => _i1.MaterialPageX<dynamic>(
-        routeData: routeData,
-        builder: (_) {
-          return const _i1.EmptyRouterPage();
-        }),
-    SearchRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
-        routeData: routeData,
-        builder: (_) {
-          return _i4.SearchPage();
-        }),
-    DetailRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
-        routeData: routeData,
-        builder: (data) {
-          final pathParams = data.pathParams;
-          final queryParams = data.queryParams;
-          final args = data.argsAs<DetailRouteArgs>(
-              orElse: () => DetailRouteArgs(
-                  id: pathParams.getString('id'),
-                  action: queryParams.optString('action'),
-                  account: queryParams.optString('account'),
-                  documentNumber: queryParams.optString('documentNumber')));
-          return _i5.DetailPage(
+  final Map<String, _i2.PageFactory> pagesMap = {
+    AppWidget.name: (routeData) {
+      final args =
+          routeData.argsAs<AppWidgetArgs>(orElse: () => const AppWidgetArgs());
+      return _i2.MaterialPageX<dynamic>(
+          routeData: routeData, child: _i1.AppWidget(key: args.key));
+    },
+    SearchRouter.name: (routeData) {
+      return _i2.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i2.EmptyRouterPage());
+    },
+    LoansRouter.name: (routeData) {
+      return _i2.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i2.EmptyRouterPage());
+    },
+    AccountRouter.name: (routeData) {
+      return _i2.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i2.EmptyRouterPage());
+    },
+    SearchRoute.name: (routeData) {
+      return _i2.MaterialPageX<dynamic>(
+          routeData: routeData, child: _i3.SearchPage());
+    },
+    DetailRoute.name: (routeData) {
+      final pathParams = routeData.inheritedPathParams;
+      final queryParams = routeData.queryParams;
+      final args = routeData.argsAs<DetailRouteArgs>(
+          orElse: () => DetailRouteArgs(
+              id: pathParams.getString('id'),
+              action: queryParams.optString('action'),
+              account: queryParams.optString('account'),
+              documentNumber: queryParams.optString('documentNumber')));
+      return _i2.MaterialPageX<dynamic>(
+          routeData: routeData,
+          child: _i4.DetailPage(
               id: args.id,
               action: args.action,
               account: args.account,
-              documentNumber: args.documentNumber);
-        }),
-    LoansRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
-        routeData: routeData,
-        builder: (_) {
-          return _i6.LoansPage();
-        }),
-    AccountRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
-        routeData: routeData,
-        builder: (_) {
-          return _i7.AccountPage();
-        }),
-    AboutRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
-        routeData: routeData,
-        builder: (_) {
-          return _i8.AboutPage();
-        })
+              documentNumber: args.documentNumber));
+    },
+    LoansRoute.name: (routeData) {
+      return _i2.MaterialPageX<dynamic>(
+          routeData: routeData, child: _i5.LoansPage());
+    },
+    AccountRoute.name: (routeData) {
+      return _i2.MaterialPageX<dynamic>(
+          routeData: routeData, child: _i6.AccountPage());
+    },
+    AboutRoute.name: (routeData) {
+      return _i2.MaterialPageX<dynamic>(
+          routeData: routeData, child: _i7.AboutPage());
+    }
   };
 
   @override
-  List<_i1.RouteConfig> get routes => [
-        _i1.RouteConfig(AppWidget.name, path: '/', children: [
-          _i1.RouteConfig(SearchRouter.name, path: 'search', children: [
-            _i1.RouteConfig(SearchRoute.name, path: ''),
-            _i1.RouteConfig(DetailRoute.name, path: ':id'),
-            _i1.RouteConfig('*#redirect',
-                path: '*', redirectTo: '', fullMatch: true)
-          ]),
-          _i1.RouteConfig(LoansRouter.name, path: 'loans', children: [
-            _i1.RouteConfig(LoansRoute.name, path: ''),
-            _i1.RouteConfig(DetailRoute.name, path: ':id'),
-            _i1.RouteConfig('*#redirect',
-                path: '*', redirectTo: '', fullMatch: true)
-          ]),
-          _i1.RouteConfig(AccountRouter.name, path: 'account', children: [
-            _i1.RouteConfig(AccountRoute.name, path: ''),
-            _i1.RouteConfig(AboutRoute.name, path: 'about'),
-            _i1.RouteConfig('*#redirect',
-                path: '*', redirectTo: '', fullMatch: true)
-          ])
+  List<_i2.RouteConfig> get routes => [
+        _i2.RouteConfig(AppWidget.name, path: '/', children: [
+          _i2.RouteConfig(SearchRouter.name,
+              path: 'search',
+              parent: AppWidget.name,
+              children: [
+                _i2.RouteConfig(SearchRoute.name,
+                    path: '', parent: SearchRouter.name),
+                _i2.RouteConfig(DetailRoute.name,
+                    path: ':id', parent: SearchRouter.name),
+                _i2.RouteConfig('*#redirect',
+                    path: '*',
+                    parent: SearchRouter.name,
+                    redirectTo: '',
+                    fullMatch: true)
+              ]),
+          _i2.RouteConfig(LoansRouter.name,
+              path: 'loans',
+              parent: AppWidget.name,
+              children: [
+                _i2.RouteConfig(LoansRoute.name,
+                    path: '', parent: LoansRouter.name),
+                _i2.RouteConfig(DetailRoute.name,
+                    path: ':id', parent: LoansRouter.name),
+                _i2.RouteConfig('*#redirect',
+                    path: '*',
+                    parent: LoansRouter.name,
+                    redirectTo: '',
+                    fullMatch: true)
+              ]),
+          _i2.RouteConfig(AccountRouter.name,
+              path: 'account',
+              parent: AppWidget.name,
+              children: [
+                _i2.RouteConfig(AccountRoute.name,
+                    path: '', parent: AccountRouter.name),
+                _i2.RouteConfig(AboutRoute.name,
+                    path: 'about', parent: AccountRouter.name),
+                _i2.RouteConfig('*#redirect',
+                    path: '*',
+                    parent: AccountRouter.name,
+                    redirectTo: '',
+                    fullMatch: true)
+              ])
         ])
       ];
 }
 
-class AppWidget extends _i1.PageRouteInfo<AppWidgetArgs> {
-  AppWidget({_i2.Key? key, List<_i1.PageRouteInfo>? children})
-      : super(name,
+/// generated route for
+/// [_i1.AppWidget]
+class AppWidget extends _i2.PageRouteInfo<AppWidgetArgs> {
+  AppWidget({_i8.Key? key, List<_i2.PageRouteInfo>? children})
+      : super(AppWidget.name,
             path: '/',
             args: AppWidgetArgs(key: key),
             initialChildren: children);
@@ -119,43 +143,58 @@ class AppWidget extends _i1.PageRouteInfo<AppWidgetArgs> {
 class AppWidgetArgs {
   const AppWidgetArgs({this.key});
 
-  final _i2.Key? key;
+  final _i8.Key? key;
+
+  @override
+  String toString() {
+    return 'AppWidgetArgs{key: $key}';
+  }
 }
 
-class SearchRouter extends _i1.PageRouteInfo {
-  const SearchRouter({List<_i1.PageRouteInfo>? children})
-      : super(name, path: 'search', initialChildren: children);
+/// generated route for
+/// [_i2.EmptyRouterPage]
+class SearchRouter extends _i2.PageRouteInfo<void> {
+  const SearchRouter({List<_i2.PageRouteInfo>? children})
+      : super(SearchRouter.name, path: 'search', initialChildren: children);
 
   static const String name = 'SearchRouter';
 }
 
-class LoansRouter extends _i1.PageRouteInfo {
-  const LoansRouter({List<_i1.PageRouteInfo>? children})
-      : super(name, path: 'loans', initialChildren: children);
+/// generated route for
+/// [_i2.EmptyRouterPage]
+class LoansRouter extends _i2.PageRouteInfo<void> {
+  const LoansRouter({List<_i2.PageRouteInfo>? children})
+      : super(LoansRouter.name, path: 'loans', initialChildren: children);
 
   static const String name = 'LoansRouter';
 }
 
-class AccountRouter extends _i1.PageRouteInfo {
-  const AccountRouter({List<_i1.PageRouteInfo>? children})
-      : super(name, path: 'account', initialChildren: children);
+/// generated route for
+/// [_i2.EmptyRouterPage]
+class AccountRouter extends _i2.PageRouteInfo<void> {
+  const AccountRouter({List<_i2.PageRouteInfo>? children})
+      : super(AccountRouter.name, path: 'account', initialChildren: children);
 
   static const String name = 'AccountRouter';
 }
 
-class SearchRoute extends _i1.PageRouteInfo {
-  const SearchRoute() : super(name, path: '');
+/// generated route for
+/// [_i3.SearchPage]
+class SearchRoute extends _i2.PageRouteInfo<void> {
+  const SearchRoute() : super(SearchRoute.name, path: '');
 
   static const String name = 'SearchRoute';
 }
 
-class DetailRoute extends _i1.PageRouteInfo<DetailRouteArgs> {
+/// generated route for
+/// [_i4.DetailPage]
+class DetailRoute extends _i2.PageRouteInfo<DetailRouteArgs> {
   DetailRoute(
       {required String id,
       String? action,
       String? account,
       String? documentNumber})
-      : super(name,
+      : super(DetailRoute.name,
             path: ':id',
             args: DetailRouteArgs(
                 id: id,
@@ -185,22 +224,33 @@ class DetailRouteArgs {
   final String? account;
 
   final String? documentNumber;
+
+  @override
+  String toString() {
+    return 'DetailRouteArgs{id: $id, action: $action, account: $account, documentNumber: $documentNumber}';
+  }
 }
 
-class LoansRoute extends _i1.PageRouteInfo {
-  const LoansRoute() : super(name, path: '');
+/// generated route for
+/// [_i5.LoansPage]
+class LoansRoute extends _i2.PageRouteInfo<void> {
+  const LoansRoute() : super(LoansRoute.name, path: '');
 
   static const String name = 'LoansRoute';
 }
 
-class AccountRoute extends _i1.PageRouteInfo {
-  const AccountRoute() : super(name, path: '');
+/// generated route for
+/// [_i6.AccountPage]
+class AccountRoute extends _i2.PageRouteInfo<void> {
+  const AccountRoute() : super(AccountRoute.name, path: '');
 
   static const String name = 'AccountRoute';
 }
 
-class AboutRoute extends _i1.PageRouteInfo {
-  const AboutRoute() : super(name, path: 'about');
+/// generated route for
+/// [_i7.AboutPage]
+class AboutRoute extends _i2.PageRouteInfo<void> {
+  const AboutRoute() : super(AboutRoute.name, path: 'about');
 
   static const String name = 'AboutRoute';
 }
